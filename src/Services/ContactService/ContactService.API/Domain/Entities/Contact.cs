152877cb01c6b170d073dc8ae71897b6da0db318
kerem <kerem@phonoclick.com>
2022-01-29 13:51:48 +0300

    created entities

---
+++ src/Services/ContactService/ContactService.API/Domain/Entities/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace ContactService.API.Domain.Entities
+{
+  public class Contact
+  {
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Company { get; set; }
+    public IList<ContactInfo> ContactInfos { get; set; } = new List<ContactInfo>();
+  }
+}
---
+++ src/Services/ContactService/ContactService.API/Domain/Entities/ContactInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace ContactService.API.Domain.Entities
+{
+  public class ContactInfo
+  {
+    public ContactInfoType InfoType { get; set; }
+    public string Value { get; set; }
+
+    public enum ContactInfoType
+    { 
+      NoType = 0,
+      Phone = 1,
+      Email = 2,
+      Location = 3
+    }
+  }
+}
