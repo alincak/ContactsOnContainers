13dcdb6e58e41dc06fb2daf765cb393f59ba688e
kerem <kerem@phonoclick.com>
2022-01-29 14:53:11 +0300

    coding repository and mongodb implemented

---
+++ src/Services/ContactService/ContactService.API/Infrastructure/Repository/ContactRepository.cs
@@ -0,0 +1,56 @@
+﻿using ContactService.API.Configurations.Settings;
+using ContactService.API.Domain.Entities;
+using MongoDB.Driver;
+
+namespace ContactService.API.Infrastructure.Repository
+{
+  public class ContactRepository : IContactRepository
+  {
+    private readonly IMongoCollection<Contact> _contactCollection;
+    private readonly IMongoCollection<ContactInfo> _contactInfoCollection;
+
+    public ContactRepository(IDatabaseSettings databaseSettings)
+    {
+      var client = new MongoClient(databaseSettings.ConnectionString);
+      var db = client.GetDatabase(databaseSettings.DatabaseName);
+
+      _contactCollection = db.GetCollection<Contact>(databaseSettings.ContactCollectionName);
+      _contactInfoCollection = db.GetCollection<ContactInfo>(databaseSettings.ContactInfoCollectionName);
+    }
+
+    public Task<ContactInfo> AddContactInfoAsync(ContactInfo.ContactInfoType infoType, string value)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<Contact> CreateAsync(Contact contact)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(string id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteContactInfoAsync(string id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<IList<Contact>> GetAllAsync()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<Contact> GetContactAsync(string id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<IList<ContactInfo>> GetContactInfosAsync(string contactId)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
--- src/Services/ContactService/ContactService.API/Program.cs
+++ src/Services/ContactService/ContactService.API/Program.cs
@@ -1,3 +1,6 @@
+using ContactService.API.Configurations.Settings;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection(nameof(DatabaseSettings)));
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+  return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
 
 var app = builder.Build();
 
