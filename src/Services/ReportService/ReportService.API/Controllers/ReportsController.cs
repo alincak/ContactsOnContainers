dd4ff565146ac74932959d0998f5e14122b60580
kerem <kerem@phonoclick.com>
2022-01-30 15:07:17 +0300

    created report service

---
+++ src/Services/ReportService/ReportService.API/Controllers/ReportsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReportService.API.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class ReportsController : ControllerBase
+  {
+
+  }
+}
