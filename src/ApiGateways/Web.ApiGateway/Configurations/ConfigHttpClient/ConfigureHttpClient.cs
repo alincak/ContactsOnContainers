3710acdbc28e35a5d4321c68d1af1bb7f6ad0548
alincak <kerem1609@gmail.com>
2022-01-31 20:34:51 +0300

    apigateway ocelot+consul

---
+++ src/ApiGateways/Web.ApiGateway/Configurations/ConfigHttpClient/ConfigureHttpClient.cs
@@ -0,0 +1,27 @@
+﻿namespace Web.ApiGateway.Configurations.ConfigHttpClient
+{
+  public class ConfigureHttpClient
+  {
+    private readonly WebApplicationBuilder builder;
+
+    public ConfigureHttpClient(WebApplicationBuilder builder)
+    {
+      this.builder = builder;
+    }
+
+    public void Setup()
+    {
+      builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+      builder.Services.AddHttpClient("contact", c =>
+      {
+        c.BaseAddress = new Uri(builder.Configuration.GetValue<string>("urls:contact"));
+      });
+
+      builder.Services.AddHttpClient("report", c =>
+      {
+        c.BaseAddress = new Uri(builder.Configuration.GetValue<string>("urls:report"));
+      });
+    }
+  }
+}
--- src/ApiGateways/Web.ApiGateway/Program.cs
+++ src/ApiGateways/Web.ApiGateway/Program.cs
@@ -1,25 +1,42 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Web.ApiGateway.Configurations.ConfigHttpClient;
+using Web.ApiGateway.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host
+  .ConfigureAppConfiguration((hostingContext, config) =>
+{
+  config.AddJsonFile("Configurations/ocelot.json");
+})
+ .UseDefaultServiceProvider((context, options) =>
+{
+  options.ValidateOnBuild = false;
+  options.ValidateScopes = false;
+});
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IContactService, ContactService>();
+builder.Services.AddScoped<IReportService, ReportService>();
+
+builder.Services.AddOcelot().AddConsul();
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("CorsPolicy",
+      builder => builder.SetIsOriginAllowed((host) => true)
+      .AllowAnyMethod()
+      .AllowAnyHeader()
+      .AllowCredentials());
+});
+
+new ConfigureHttpClient(builder).Setup();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-  app.UseSwagger();
-  app.UseSwaggerUI();
-}
+app.UseCors("CorsPolicy");
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+await app.UseOcelot();
 
 app.Run();
