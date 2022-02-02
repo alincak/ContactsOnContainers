9cfde2bfe90124b9d86139d978ed74ed7adbc4ce
alincak <kerem1609@gmail.com>
2022-02-02 09:57:32 +0300

    report service mapper map profile

---
+++ src/Services/ReportService/ReportService.API/Mapping/GeneralMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ReportService.API.Domain.Entities;
+using ReportService.API.Models;
+
+namespace ContactService.API.Mapping
+{
+  public class GeneralMapping : Profile
+  {
+    public GeneralMapping()
+    {
+      CreateMap<Report, ReportVo>().ReverseMap();
+      CreateMap<ReportDetail, ReportDetailsVo>().ReverseMap();
+    }
+  }
+}
